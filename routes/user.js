4cb350b4ac7622293b88b6f316d3aae81063a9e8
BetterLordWilliam <willjordan05@icloud.com>
2025-10-31 20:32:57 -0700

    WO: reverted those goofy ahh changes made to the user router.

--- routes/user.js
+++ routes/user.js
@@ -7,111 +7,77 @@ router.use(express.json());
 
 router.route('/')
     .get(async (req, res) => {
-        try {
-            const db    = req.services.dbService;
-            const users = await db.getUser(); 
+        const db    = req.services.dbService;
+        const users = await db.getUser(); 
 
-            res.status(200);
-            res.json({
-                message: 'Successfully got all users in the database.',
-                data: users
-            });
-        } catch (err) {
-            res.status(500);
-            res.json({
-                message: ServerStrings.DB_QUERY_FAILED,
-                error: err.message
-            });
-        }
+        res.status(200);
+        res.json({
+            message: 'Successfully got all users in the database.',
+            data: users
+        });
     })
     .post(async (req, res) => {
-        try {
-            const db = req.services.dbService;
-            const newUser = req.body;
+        const db = req.services.dbService;
+        const newUser = req.body;
 
-            if (!newUser.fname || !newUser.lname  || !newUser.email || !newUser.pass) {
-                res.status(400);
-                res.send({
-                    message: ServerStrings.DB_MISSING_PARAMS('user'),
-                    missingParams: db.expectedUser
-                });
-            } else {
-                const dbRes = await db.addUser(newUser);        // Probably should do something with this
-                res.status(200);
-                res.json({
-                    message: ServerStrings.DB_QUERY_SUCCESS,
-                    data: dbRes
-                });
-            }
-
-        } catch (err) {
-            res.status(500);
+        if (!newUser.fname || !newUser.lname  || !newUser.email || !newUser.pass) {
+            res.status(400);
+            res.send({
+                message: ServerStrings.DB_MISSING_PARAMS('user'),
+                missingParams: db.expectedUser
+            });
+        } else {
+            const dbRes = await db.addUser(newUser);        // Probably should do something with this
+            res.status(200);
             res.json({
-                message: 'Database query failed.'
-            })
+                message: ServerStrings.DB_QUERY_SUCCESS,
+                data: dbRes
+            });
         }
     });
 
 router.route('/:id')
     .get(async (req, res) => {
-        try {
-            const db    = req.services.dbService;
-            const user  = await db.getUser(req.params.id);
-            
-            if (!user || user.length === 0) {
-                res.status(400);
-                res.json({
-                    message: `No user could be found for id \'${req.params.id}\'`
-                });
-
-            } else {
-                res.status(200);
-                res.json({
-                    message: `Successfully found user for id \'${req.params.id}\'`,
-                    data: user
-                });
-            }
-        } catch (err) {
-            res.status(500);
+        const db    = req.services.dbService;
+        const user  = await db.getUser(req.params.id);
+        
+        if (!user || user.length === 0) {
+            res.status(400);
             res.json({
-                message: req.services.strings.dbQueryFailed;
-            })
+                message: `No user could be found for id \'${req.params.id}\'`
+            });
+
+        } else {
+            res.status(200);
+            res.json({
+                message: `Successfully found user for id \'${req.params.id}\'`,
+                data: user
+            });
         }
     })
     .put(async (req, res) => {
-        try {
-            const db        = req.services.dbService;
-            const userData  = req.body;
-
-            if (!req.params.id) {
-                res.status(400);
-                res.json({
-                    message: ServerStrings.DB_MISSING_ID,
-                    id: req.params.id
-                });
-            } else if (!userData) {
-                res.status(400);
-                res.json({
-                    message: ServerStrings.DB_NOTHING_UPDATE,
-                    id: req.params.id
-                });
-            } else {
-                console.log(userData);
-
-                const resp = await db.updateUser(req.params.id, userData);
-                
-                res.status(200);
-                res.json({
-                    message: ServerStrings.DB_UPDATE_SUCCESS,
-                    id: req.params.id,
-                    data: resp
-                });
-            }
-        } catch (err) {
-            res.status(500);
+        if (!req.params.id) {
+            res.status(400);
             res.json({
-                message: ServerStrings.DB_QUERY_FAILED,
-                error: err.message
+                message: ServerStrings.DB_MISSING_ID,
+                id: req.params.id
+            });
+        } else if (!userData) {
+            res.status(400);
+            res.json({
+                message: ServerStrings.DB_NOTHING_UPDATE,
+                id: req.params.id
+            });
+        } else {
+            console.log(userData);
+
+            const resp = await db.updateUser(req.params.id, userData);
+            
+            res.status(200);
+            res.json({
+                message: ServerStrings.DB_UPDATE_SUCCESS,
+                id: req.params.id,
+                data: resp
             });
         }
     })
